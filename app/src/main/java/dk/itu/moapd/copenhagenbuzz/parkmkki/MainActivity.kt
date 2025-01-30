e2015cddd8a4828f0724eff22a7ab513095c8be9
Marcus Kofoed Kirkegaard <kofoedkirkegaard@icloud.com>
2025-01-30 21:17:57 +0100

    01.02 adding remaining ui elements

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/MainActivity.kt
@@ -1,18 +1,22 @@
 package dk.itu.moapd.copenhagenbuzz.parkmkki
 
-import android .os. Bundle
-import androidx . appcompat .app. AppCompatActivity
+import android.icu.text.SimpleDateFormat
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import dk.itu.moapd.copenhagenbuzz.parkmkki.databinding.ActivityMainBinding
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity () {
 
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate ( savedInstanceState : Bundle ?) {
-        super. onCreate ( savedInstanceState )
+        super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding . inflate ( layoutInflater )
-        setContentView ( binding . root )
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
 }
