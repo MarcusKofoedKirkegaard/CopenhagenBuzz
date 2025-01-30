6dee36f03908cc03e5011a40782228549cd2a098
Marcus Kofoed Kirkegaard <kofoedkirkegaard@icloud.com>
2025-01-30 21:55:21 +0100

    01.03 Initialized remaining attributes and implemented remaning accessors and mutators in Event.kt

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/Event.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/Event.kt
@@ -1,14 +1,21 @@
 package dk.itu.moapd.copenhagenbuzz.parkmkki
 
+import java.util.Date
+
 class Event {
     private var eventName: String
     private var eventLocation: String
+    private var eventDate: Date // Maybe just string?
+    private var eventType: String // Probably should be an ENUM?
+    private var eventDescription: String
 
-    constructor(eventName: String, eventLocation: String) {
+
+    constructor(eventName: String, eventLocation: String, eventDate: Date, eventType: String, eventDescription: String) {
         this.eventName = eventName
         this.eventLocation = eventLocation
-
-    // Initialize the missing attributes .
+        this.eventDate = eventDate
+        this.eventType = eventType
+        this.eventDescription = eventDescription
     }
 
     fun getEventName(): String {
@@ -28,8 +35,30 @@ class Event {
         this.eventLocation = eventLocation
     }
 
+    fun getEventDate(): Date {
+        return eventDate
+    }
 
-    // Implement the missing accessors and mutators methods .
+    fun setEventDate(eventDate: Date) {
+        this.eventDate = eventDate
+    }
+
+    fun getEventType(): String {
+        return eventType;
+    }
+
+    fun setEventType(eventType: String) {
+        this.eventType = eventType
+    }
+
+    fun getEventDescription(): String {
+        return eventDescription
+    }
+
+    fun setEventDescription(eventDescription: String) {
+        this.eventDescription = eventDescription
+    }
+
     override fun toString(): String {
         return " Event ( eventName =' $eventName ', eventLocation =' $eventLocation ')"
     }
