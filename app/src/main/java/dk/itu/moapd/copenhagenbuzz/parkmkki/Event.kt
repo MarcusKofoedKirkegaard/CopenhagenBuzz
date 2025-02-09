80bc1407614e5cb4a5a8f2f97598252306526736
Marcus Kofoed Kirkegaard <kofoedkirkegaard@icloud.com>
2025-02-09 21:01:11 +0100

    02.02 Fixed compile errors for event, after refactoring to data class

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/Event.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/Event.kt
@@ -20,80 +20,13 @@ import java.time.LocalDate
  *@see "https://kotlinlang.org/docs/data-classes.html#properties-declared-in-the-class-body"
  */
 data class Event(
-    var eventName: String,
-    var eventLocation: String,
+    var eventName: String ="",
+    var eventLocation: String="",
     var eventDate: LocalDate,
-    var eventType: String,
-    var eventDescription: String
+    var eventType: String="",
+    var eventDescription: String=""
 ) {
-    /**
-     * @return eventName: String
-     */
-    fun getEventName(): String {
-        return eventName
-    }
-    /**
-     * @param eventName
-     */
-    fun setEventName(eventName: String) {
-        this.eventName = eventName
-    }
 
-    /**
-     * @return eventLocation: String
-     */
-    fun getEventLocation(): String {
-        return eventLocation
-    }
-
-    /**
-     * @param eventLocation
-     */
-    fun setEventLocation(eventLocation: String) {
-        this.eventLocation = eventLocation
-    }
-
-    /**
-     * @return eventDate: LocalDate
-     */
-    fun getEventDate(): LocalDate {
-        return eventDate
-    }
-
-    /**
-     * @param eventDate
-     */
-    fun setEventDate(eventDate: LocalDate) {
-        this.eventDate = eventDate
-    }
-
-    /**
-     * @return eventType: String
-     */
-    fun getEventType(): String {
-        return eventType
-    }
-
-    /**
-     * @param eventType
-     */
-    fun setEventType(eventType: String) {
-        this.eventType = eventType
-    }
-
-    /**
-     * @return eventName: String
-     */
-    fun getEventDescription(): String {
-        return eventDescription
-    }
-
-    /**
-     * @param eventDescription
-     */
-    fun setEventDescription(eventDescription: String) {
-        this.eventDescription = eventDescription
-    }
 
     /**
      * Prints the Event information
