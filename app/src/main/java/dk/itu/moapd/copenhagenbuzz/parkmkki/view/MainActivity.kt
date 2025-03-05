f7ac516c7c8deadc10d255b8e7bacb3a0743be56
Marcus Kofoed Kirkegaard <kofoedkirkegaard@icloud.com>
2025-03-05 22:37:13 +0100

    04.03-04.04 Added Navigation

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/view/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/view/MainActivity.kt
@@ -10,6 +10,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import dk.itu.moapd.copenhagenbuzz.parkmkki.databinding.ActivityMainBinding
 import dk.itu.moapd.copenhagenbuzz.parkmkki.R
 
@@ -18,8 +22,10 @@ import dk.itu.moapd.copenhagenbuzz.parkmkki.R
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -36,11 +42,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setSupportActionBar(binding.contentMain.topAppBar)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.contentMain.bottomNavigation, navController)
+
+
+
+        // Listen for changes in selected fragment and update title accordingly
+
         // Load the fragment into fragment_container
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, AddEventFragment())
                 .commit()
-        }
+        }*/
     }
 }
