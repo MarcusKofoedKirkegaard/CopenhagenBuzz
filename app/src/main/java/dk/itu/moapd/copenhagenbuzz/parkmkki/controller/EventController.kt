c2958a5fedd822f212d58e4964102a839d6f2bad
Marcus Kofoed Kirkegaard <kofoedkirkegaard@icloud.com>
2025-03-09 19:57:58 +0100

    Assignment 1 Polish - Complete docstring documentation for EventController.kt

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/controller/EventController.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/parkmkki/controller/EventController.kt
@@ -1,23 +1,34 @@
+/*
+ * (License Notice)
+ * MIT License
+ * Copyright (c) [2025] [Emil Parkel & Marcus Kofoed Kirkegaard]
+ * See README for more
+ */
 package dk.itu.moapd.copenhagenbuzz.parkmkki.controller
 
 import dk.itu.moapd.copenhagenbuzz.parkmkki.model.Event
 import java.time.LocalDate
 
 /**
- * Event Controller to manage the event class
+ * **EventController** is responsible for managing an instance of the [Event] model.
+ * It provides methods to update and retrieve event data.
  */
 class EventController {
 
+    /**
+     * A single instance of an [Event] used for temporary storage.
+     * Defaults to an empty event with the current date.
+     */
     private val event = Event("", "", LocalDate.now(), "", "", 0)
 
     /**
-     * Update event
+     * Updates the stored event instance with new details.
      *
-     * @param name
-     * @param location
-     * @param date
-     * @param type
-     * @param description
+     * @param name The name of the event.
+     * @param location The location where the event takes place.
+     * @param date The date of the event in [LocalDate] format.
+     * @param type The type or category of the event.
+     * @param description A brief description of the event.
      */
     fun updateEvent(name: String, location: String, date: LocalDate, type: String, description: String) {
         event.eventName = name
@@ -28,9 +39,9 @@ class EventController {
     }
 
     /**
-     * Gets event
+     * Retrieves the current event instance.
      *
-     * @return Event
+     * @return The stored [Event] object containing the latest event details.
      */
     fun getEvent(): Event {
         return event
